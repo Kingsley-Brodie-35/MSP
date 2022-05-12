777c88274366ac6f177f95b12fe8fc084dfec011
Kingsley Brodie <kingsleybrodie4@gmail.com>
2022-05-13 00:15:13 +1000

    basic sign in at startup configured correctly

--- HospRec/Startup.cs
+++ HospRec/Startup.cs
@@ -3,13 +3,14 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.EntityFrameworkCore;
+//using Microsoft.EntityFrameworkCore;
 using HospRec.Data;
 using Microsoft.Identity.Web.UI;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.Identity.Web;
+
 
 
 namespace HospRec
@@ -23,11 +24,40 @@ namespace HospRec
 
         public IConfiguration Configuration { get; }
 
+       
+
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            //azure ad config
-            services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme).AddMicrosoftIdentityWebApp(Configuration.GetSection("AzureAd"));
+
+            ////azure ad config
+            //services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme).AddMicrosoftIdentityWebApp(Configuration.GetSection("AzureAd"));
+            //services.AddControllersWithViews(options =>
+            //{
+            //    var policy = new AuthorizationPolicyBuilder()
+            //        .RequireAuthenticatedUser()
+            //        .Build();
+            //    options.Filters.Add(new AuthorizeFilter(policy));
+            //});
+            ////db connection
+            //services.Add(new ServiceDescriptor(typeof(DbClass), new DbClass(Configuration.GetConnectionString("DefaultConnection"))));
+            ////authorize accounts
+            //services.AddAuthorization(options =>
+            //{
+            //    options.AddPolicy("doctor", policyBuilder => policyBuilder.RequireClaim("groups", "8ce5badf-d1bf-48ba-8344-7dd0951102a4"));
+            //});
+            ////add razor pages
+            //services.AddRazorPages().AddMicrosoftIdentityUI();
+
+
+            var initialScopes = Configuration.GetValue<string>("DownstreamApi:Scopes")?.Split(' ');
+
+            services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+                .AddMicrosoftIdentityWebApp(Configuration.GetSection("AzureAd"))
+                    .EnableTokenAcquisitionToCallDownstreamApi(initialScopes)
+                        .AddMicrosoftGraph(Configuration.GetSection("DownstreamApi"))
+                        .AddInMemoryTokenCaches();
+
             services.AddControllersWithViews(options =>
             {
                 var policy = new AuthorizationPolicyBuilder()
@@ -35,9 +65,9 @@ namespace HospRec
                     .Build();
                 options.Filters.Add(new AuthorizeFilter(policy));
             });
-            //db connection
-            services.Add(new ServiceDescriptor(typeof(DbClass), new DbClass(Configuration.GetConnectionString("DefaultConnection"))));
-            services.AddRazorPages().AddMicrosoftIdentityUI();
+            services.AddRazorPages()
+                 .AddMicrosoftIdentityUI();
+
         }
         
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
