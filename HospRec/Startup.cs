41aebaf8e70161544c36fbb28c8e3ce1f1f7092a
Kingsley Brodie <kingsleybrodie4@gmail.com>
2022-05-12 19:12:23 +1000

    azure ad integration incomplete

--- HospRec/Startup.cs
+++ HospRec/Startup.cs
@@ -6,6 +6,12 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using HospRec.Data;
+using Microsoft.Identity.Web.UI;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Identity.Web;
+
 namespace HospRec
 {
     public class Startup
@@ -20,12 +26,18 @@ namespace HospRec
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Add(new ServiceDescriptor(typeof(DbClass), new DbClass(Configuration.GetConnectionString("DefaultConnection"))));
-            services.AddRazorPages().AddRazorPagesOptions(options =>
+            //azure ad config
+            services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme).AddMicrosoftIdentityWebApp(Configuration.GetSection("AzureAd"));
+            services.AddControllersWithViews(options =>
             {
-                options.Conventions.AddPageRoute("/edit/{patientID:int?}", "/search/{patientID}/edit/{patientID}");
+                var policy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+                options.Filters.Add(new AuthorizeFilter(policy));
             });
-
+            //db connection
+            services.Add(new ServiceDescriptor(typeof(DbClass), new DbClass(Configuration.GetConnectionString("DefaultConnection"))));
+            services.AddRazorPages().AddMicrosoftIdentityUI();
         }
         
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -52,6 +64,10 @@ namespace HospRec
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}"
+               );
                 endpoints.MapRazorPages();
             });
         }
