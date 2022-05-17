c86c5a3149740eb7752397d3499210ae2836868f
jazteng2 <jazteng2@github.com>
2022-05-17 17:18:25 +1000

    Validation Fix, Modified Doctor Model

--- HospRec/Models/Patient.cs
+++ HospRec/Models/Patient.cs
@@ -1,32 +1,34 @@
-﻿using System;  
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace HospRec.Models
 {
     public class Patient
-    {   
+    {
         //fields
         [Key]
-        public int Id {get; set;}
+        public int Id { get; set; }
         [Required]
         [StringLength(15, ErrorMessage = "Last name must be 15 characters long")]
-        public string firstName {get; set;}
+        [RegularExpression(@"[a-zA-Z]", ErrorMessage = "First name must be alphebetical")]
+        public string firstName { get; set; }
         [Required]
         [StringLength(15, ErrorMessage = "Last name must be 15 characters long")]
-        public string lastName {get; set;}
+        [RegularExpression(@"[a-zA-Z]", ErrorMessage = "First name must be alphebetical")]
+        public string lastName { get; set; }
         [Required]
         [DataType(DataType.EmailAddress)]
-        public string email {get; set;}
+        public string email { get; set; }
         [Required]
         [RegularExpression(@"[0-9]{10}", ErrorMessage = "Phone number must be 10 digits long")]
-        public string phoneNumber {get; set;}
+        public string phoneNumber { get; set; }
         [Required]
         [RegularExpression(@"[^-]", ErrorMessage = "Invalid Gender")]
-        public char gender {get; set;}
-        [Required]        
+        public char gender { get; set; }
+        [Required]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
-        public string DOB {get; set;}
+        public string DOB { get; set; }
     }
 }
