12dcfa4d4da44d63d5c4674e0743ee1505505640
Kingsley Brodie <kingsleybrodie4@gmail.com>
2022-06-21 14:31:31 +1000

    completed addPatientRecord

--- HospRec/Models/PatientRecord.cs
+++ HospRec/Models/PatientRecord.cs
@@ -5,9 +5,14 @@ namespace HospRec.Models
 {
     public class PatientRecord
     {
-        
+        [Key]
         public int Record_ID { get; set; }
+        [Key]
+        [Required]
+        [RegularExpression(@"[0-9]", ErrorMessage = "Must be a number")]
         public int Patient_ID { get; set; }
+        [Key]
+        [RegularExpression(@"[0-9]", ErrorMessage = "Must be a number")]
         public int Doctor_ID { get; set; }
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}")]
