7ab3cf875389f1c34e64601d74f941b1aeb5beca
jazteng2 <jaredteng1602@gmail.com>
2022-06-28 13:18:11 +1000

    -

---
+++ HospRec/Pages/SearchPatientRecords.cshtml.cs
@@ -0,0 +1,92 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using HospRec.Data;
+using HospRec.Models;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace HospRec.Pages
+{
+    [BindProperties]
+    public class SearchPatientRecordsModel : PageModel
+    {
+        //search 
+        public string Record_ID { get; set;}
+
+        public string Patient_ID { get; set; }
+
+        public string Doctor_ID { get; set; }
+
+        public string P_First_Name { get; set; }
+
+        public string P_Last_Name { get; set; }
+
+        public string D_First_Name { get; set; }
+
+        public string D_Last_Name { get; set; }
+
+
+        private PatientRecordContext PatientRecordContext;
+
+        public List<PatientRecord> Records = new List<PatientRecord>();
+
+        public SearchPatientRecordsModel(PatientRecordContext prc)
+        {
+            PatientRecordContext = prc;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public IActionResult OnPost()
+        {
+            string sqlWhere = "WHERE ";
+            if (Record_ID != null)
+            {
+                sqlWhere += $"patient_record.Record_ID = \"{Record_ID}\" AND ";
+            }
+            if (Patient_ID != null)
+            {
+                sqlWhere += $"patient_record.Patient_ID = \"{Patient_ID}\" AND ";
+            }
+            if (Doctor_ID != null)
+            {
+                sqlWhere += $"patient_record.Doctor_ID = \"{Doctor_ID}\" AND ";
+            }
+            if (P_First_Name != null)
+            {
+                sqlWhere += $"patient.FirstName = \"{P_First_Name}\" AND ";
+            }
+            if (P_Last_Name != null)
+            {
+                sqlWhere += $"patient.LastName = \"{P_First_Name}\" AND ";
+            }
+            if (D_First_Name != null)
+            {
+                sqlWhere += $"doctor.FirstName = \"{D_First_Name}\" AND ";
+            }
+            if (D_Last_Name != null)
+            {
+                sqlWhere += $"doctor.LastName = \"{D_Last_Name}\" AND ";
+            }
+            if (sqlWhere.Length > 6)
+            {
+                sqlWhere = sqlWhere.Remove(sqlWhere.Length - 4, 4);
+            }
+            //no parameters entered return page
+            else
+            {
+                ViewData["Results"] = "please enter a search parameter";
+                return Page();
+            }
+            sqlWhere.TrimEnd();
+            Records = PatientRecordContext.getPatientRecords(sqlWhere);
+            if (Records.Count == 0)
+            {
+                ViewData["Results"] = "No results returned";
+            }
+            return Page();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# MSP
+HospRec is a Hospital Software Solution developed for Doctor's in hospital clinics.
+HospRec allows Doctors to sign into an Azure Active Directory account set up by an administrator in which they can gain access to an array of functionality designed 
+to help doctors keep track of patient's and patient records.
+
+Doctors have the ability to.
+  Add, edit and search for Patients and Patient Records.
+ 
+Data is stored in the cloud using Azure Database for MySQL and the application was hosted using Azure App Services whilst single sign on was abstracted using Azure 
+Active Directory.
+
+The technology stack used in this project is C#, ASP.NET, Razor Pages, HTML, CSS, JS, MySQL.
