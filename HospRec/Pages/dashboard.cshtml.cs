4c5981bef89adaf0d038eeb760cf46ecf4ecebf8
jazteng2 <jaredteng1602@gmail.com>
2022-07-07 21:36:08 +1000

    Added doctor dashbaord pages

---
+++ HospRec/Pages/dashboard.cshtml.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Principal;
+using System.Security.Claims;
+
+
+namespace HospRec.Pages
+{ 
+    public class dashboardModel : PageModel
+    {
+
+        public dashboardModel()
+        {
+        }
+
+        public void OnGet()
+        {
+          
+        }
+    }
+}
