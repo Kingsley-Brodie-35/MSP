09010ac2dcc0d1b085a1916b96f5c3b448e2ed09
jazteng2 <jaredteng1602@gmail.com>
2022-08-10 18:10:42 +1000

    FIX: Dropdown seperation, color correction

--- HospRec/wwwroot/js/site.js
+++ HospRec/wwwroot/js/site.js
@@ -1,19 +1,26 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-// Nav functions
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
-      }
+function clearDropdown() {
+  var dropdowns = document.getElementsByClassName("dropdown-content");
+      
+  for (i = 0; i < dropdowns.length; i++) {
+    var openDropdown = dropdowns[i];
+    if (openDropdown.classList.contains('show')) {
+      openDropdown.classList.remove('show');
     }
   }
+}
+window.onclick = function(event) {
+  if (event.target.matches('.dropbtn')) {
+    clearDropdown(); // clear existing dropdowns on screen
+    if (event.target.matches('.search')) {
+      document.getElementById("dropdown-search").classList.toggle("show");
+    }
+    if (event.target.matches('.add')) {
+      document.getElementById("dropdown-add").classList.toggle("show");
+    }
+  } else {
+    clearDropdown();
+  }
+}
