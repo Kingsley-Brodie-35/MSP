c1f9cabb5dfe0df0941a1f6705e5c85305717474
Jared Teng <jaredteng2002@gmail.com>
2022-08-11 22:00:18 +1000

    FIX: color consistency between button and dropdown

--- HospRec/wwwroot/js/site.js
+++ HospRec/wwwroot/js/site.js
@@ -1,26 +1,30 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
 
-function clearDropdown() {
-  var dropdowns = document.getElementsByClassName("dropdown-content");
+function clearClassName(targetClass, clearClass) {
+  var dropdowns = document.getElementsByClassName(targetClass);
       
   for (i = 0; i < dropdowns.length; i++) {
     var openDropdown = dropdowns[i];
-    if (openDropdown.classList.contains('show')) {
-      openDropdown.classList.remove('show');
+    if (openDropdown.classList.contains(clearClass)) {
+      openDropdown.classList.remove(clearClass);
     }
   }
 }
 window.onclick = function(event) {
   if (event.target.matches('.dropbtn')) {
-    clearDropdown(); // clear existing dropdowns on screen
+    clearClassName("dropdown-content", "show"); // clear existing dropdowns on screen
+    clearClassName("dropbtn", "fix"); // clear fix colors on dropdown button
     if (event.target.matches('.search')) {
+      document.getElementById("dropSearch").classList.toggle("fix");
       document.getElementById("dropdown-search").classList.toggle("show");
     }
     if (event.target.matches('.add')) {
+      document.getElementById("dropAdd").classList.toggle("fix");
       document.getElementById("dropdown-add").classList.toggle("show");
     }
   } else {
-    clearDropdown();
+    clearClassName("dropdown-content", "show");
+    clearClassName("dropbtn", "fix");
   }
 }
