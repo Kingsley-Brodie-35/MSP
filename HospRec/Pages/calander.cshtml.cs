703fbd140759d36f5c3f3eb786834f4d8d85acc7
jazteng2 <jaredteng1602@gmail.com>
2022-08-13 01:07:07 +1000

    initial setup calander

---
+++ HospRec/Pages/calander.cshtml.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Security.Principal;
+using System.Security.Claims;
+
+
+namespace HospRec.Pages
+{ 
+    public class CalanderModel : PageModel
+    {
+
+        public CalanderModel()
+        {
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
